5d3fa641a67e1406bdc89bb3cc010962e9293707
kaoru.shiraga <karats24.kaoru@gmail.com>
2024-03-11 07:38:51 +0900

    メニューアイテムデータ作成

---
+++ app/menuItemsData.ts
@@ -0,0 +1,32 @@
+export const menuItemsData = [
+    {
+      title: 'Collections',
+      url: '/collections',
+      submenu: [
+        {
+          title: 'First Collection',
+          url: '/first-collection',
+        },
+      ],
+    },
+    {
+        title: 'About',
+        url: '/about',
+        submenu: [
+            {
+                title: 'About Seika Shiraga',
+                url: '/about-seika-shiraga',
+            },
+        ],
+    },
+    {
+        title: 'Contact',
+        url: '/contact',
+        submenu: []
+    },
+    {
+        title: 'Online Store',
+        url: 'https://seikashiraga.official.ec/',
+        submenu: []
+    },
+];
